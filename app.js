935ff43f20c6361ec77ade1dc79e62f04bd0f42b
utorgo <utorgo@gmail.com>
2015-06-17 17:08:39 +0300

    1.7 Look, More Gems!

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 /**
  * Created by Аня on 17.06.2015.
  */
-(function(){
-    var gem = { name: 'Azurite', price: 2.95 };
+(function() {
     var app = angular.module('gemStore', []);
-    app.controller('StoreController',function(){
-        this.product = gem;
+
+    app.controller('StoreController', function(){
+        this.products=gems;
     });
+
+    var gems = [
+        { name: 'Azurite', price: 2.95 },
+        { name: 'Bloodstone', price: 5.95 },
+        { name: 'Zircon', price: 3.95 }
+    ];
 })();
