598ee4a20fb974630049c46907b447847c0ab81d
utorgo <utorgo@gmail.com>
2015-06-17 16:41:53 +0300

    1.4 Our First Controller

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 /**
  * Created by Аня on 17.06.2015.
  */
-var app = angular.module('gemStore',[]);
+(function(){
+    var gem = { name: 'Azurite', price: 2.95 };
+    var app = angular.module('gemStore', []);
+    app.controller('StoreController',function(){
+        this.product = gem;
+    });
+})();
